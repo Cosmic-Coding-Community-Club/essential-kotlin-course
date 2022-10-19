5f025ee0ffc21b307079eea5c989d87bea6ca131
jordi-jimenez <jordi.jimenez@adevinta.com>
2022-10-19 16:41:20 +0200

    Readme and license of project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.7.20"
 }
 
 group = "com.4c"
@@ -23,5 +23,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
