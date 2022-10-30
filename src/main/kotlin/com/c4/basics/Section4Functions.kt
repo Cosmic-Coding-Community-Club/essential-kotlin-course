e1c033097d4f923c9c2c6e173f992d350103e54d
jordi-jimenez <jordi.jimenez@adevinta.com>
2022-10-30 15:09:38 +0100

    remove solutions of functions exercises

--- src/main/kotlin/com/c4/basics/Section4Functions.kt
+++ src/main/kotlin/com/c4/basics/Section4Functions.kt
@@ -3,25 +3,22 @@ package com.c4.basics
 object Section4Functions {
     
     // Define function that has an integer param and returns a string
-    fun task1(aInt: Int): String {
-        return "This is a string example"
+    fun task1() {
     }
     
     // Define function that has one string param and returns any value
-    fun task2(aString: String) {
+    fun task2() {
     }
     
     // Define function that has two short integer param and returns any value
-    fun task3(aShort: Short, aOtherShort: Short) {
+    fun task3() {
     }
     
     // Define function that has three param, one double, one string and one float and returns a long
-    fun task4(aDouble: Double, aString: String, aFloat: Float): Long {
-        return 2992L
+    fun task4() {
     }
     
     // Define function that can have undefine number of params type of byte and returns an integer
-    fun task5(vararg aByteParams: Byte): Int {
-        return 2
+    fun task5() {
     }
 }
