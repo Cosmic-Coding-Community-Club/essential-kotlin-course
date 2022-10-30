634643e1d1cdd7178cd3073e39182a2786615171
jordi-jimenez <jordi.jimenez@adevinta.com>
2022-10-30 12:53:55 +0100

    remove solutions

--- src/main/kotlin/com/c4/basics/Section1Variable.kt
+++ src/main/kotlin/com/c4/basics/Section1Variable.kt
@@ -4,36 +4,36 @@ object Section1Variable {
     
     // Return integer numeric variable
     fun task1(): Any {
-        return 1982
+        return Any()
     }
 
     // Return string variable
     fun task2(): Any {
-        return "Jordi"
+        return Any()
     }
 
     // Return character variable
     fun task3(): Any {
-        return 'C'
+        return Any()
     }
 
     // Return short precision floating-point variable
     fun task4(): Any {
-        return 182.5f
+        return Any()
     }
 
     // Return large precision floating-point variable
     fun task5(): Any {
-        return 3.141592654
+        return Any()
     }
 
     // Return boolean variable
     fun task6(): Any {
-        return true
+        return Any()
     }
 
     // Return array of characters
     fun task7(): Any {
-        return arrayOf('c', 'o', 's', 'm', 'i', 'c')
+        return Any()
     }
 }
