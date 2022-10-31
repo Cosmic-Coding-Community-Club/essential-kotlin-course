67c41debedcbb05b647d5ef1ce2ad2bc3a35783e
jordi-jimenez <jordi.jimenez@adevinta.com>
2022-10-31 16:59:08 +0100

    remove solutions

--- src/main/kotlin/com/c4/basics/Section5NamedArgumentsAndDefaults.kt
+++ src/main/kotlin/com/c4/basics/Section5NamedArgumentsAndDefaults.kt
@@ -9,20 +9,20 @@ object Section5NamedArgumentsAndDefaults {
     var argumentCapture: Array<Any> = arrayOf(-1, -1)
     
     // Define function that has an integer param with given default value and returns any value
-    fun task1(aInt: Int = GIVEN_A_INT) {
-        argumentCapture[0] = aInt
+    fun task1() {
+        argumentCapture[0] = -1
     }
     
     // Define function that has a String and a float param with given default values and returns any value
-    fun task2(aString: String = GIVEN_A_STRING, afloat: Float = GIVEN_A_FLOAT) {
-        argumentCapture[0] = aString
-        argumentCapture[1] = afloat
+    fun task2() {
+        argumentCapture[0] = -1
+        argumentCapture[1] = -1
     }
     
     // Call function "functionToCall" passing arguments in different order
     // and assign returned bool value to argumentCapture
     fun task3() {
-        argumentCapture[0] = functionToCall(aLong = GIVEN_A_LONG, aString = GIVEN_A_STRING)
+        argumentCapture[0] = -1
     }
     
     private fun functionToCall(aString: String, aLong: Long): Boolean {
