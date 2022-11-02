602d4bd9579b497bea8ec2d6f7782819476d2888
jordi-jimenez <jordi.jimenez@adevinta.com>
2022-11-02 08:41:21 +0100

    remove section 8

--- src/main/kotlin/com/c4/basics/Section8Exceptions.kt
+++ src/main/kotlin/com/c4/basics/Section8Exceptions.kt
@@ -9,24 +9,16 @@ object Section8Exceptions {
     // Define the behavior of method:
     // throw a Runtime Exception with message MESSAGE_USER_NOT_FOUND
     fun task1() {
-        throw RuntimeException(MESSAGE_USER_NOT_FOUND)
     }
     
     // Define the behavior of method:
     // throw a Null Pointer Exception
     fun task2() {
-        throw NullPointerException()
     }
     
     // Define the behavior of method:
     // avoid the Null Pointer Exception throw from "wrongFunction" and return DEFAULT_STRING
-    fun task3(): String {
-        return try {
-            wrongFunction()
-        } catch (nullPointerException: NullPointerException) {
-            DEFAULT_STRING
-        }
-    }
+    fun task3(): String = wrongFunction()
     
     private fun wrongFunction(): String {
         throw NullPointerException()
